3199705a8304cadabb79626644824935db206755
rebeccalim20 <s10207898@connect.np.edu.sg>
2021-01-10 01:25:18 +0800

    updated on bus fare data and bus data infos

--- js/index.js
+++ js/index.js
@@ -75,7 +75,8 @@ $(document).ready(function () {
           
           responseSummary += '<tr>' + '<td class="service-no">' +'<span class="text">'
           + obj['ServiceNo'] + '</span>'+'<br>' + '</td>' + '<td>' + obj['AM_Peak_Freq'] + '<br>' + '</td>' 
-          + '<td>' + obj['AM_Offpeak_Freq'] + '<br>' + '</td>' +'</tr>' 
+          + '<td>' + obj['AM_Offpeak_Freq'] + '<br>' + '</td>' + '<td>' + obj['PM_Peak_Freq'] 
+          + '<br>' + '</td>' + '<td>' +obj['PM_Offpeak_Freq'] + '<br>' + '</td>' +'</tr>' 
         }
         $("#bus-content").html(responseSummary);
   
@@ -85,48 +86,5 @@ $(document).ready(function () {
     }
 
 
-  
-    // function findBus(param) {
-    //   var settings = {
-    //     "url": "https://cors-anywhere.herokuapp.com/http://datamall2.mytransport.sg/ltaodataservice/BusServices",
-    //     "method": "GET",
-    //     async: true,
-  
-    //     crossDomain: true,
-    //     "timeout": 0,
-    //     "headers": {
-    //       "AccountKey": "ly9L23Y+QCmAMhVZGbbRtA==", //Use your own Account Key
-    //     },
-    //   };
-  
-    //   //ACCOUNTKEY = wX12O8DoTAuIZ9/3fMyKWQ==
-    //   //UNIQUEUSERID = 438ffd3c-51ef-4ccb-bb52-aa7014a236fb
-  
-    //   $.ajax(settings).done(function (response) {
-  
-    //     console.log(response.value);
-    //     // let a = response.value;
-        
-  
-    //     // console.log(JSON.stringify(settings));
-    //     let responseSummary = "";
-    //     // console.log(response.value);
-  
-    //     for (var i = 0; i < response.value.length; i++) {
-    //       var obj = response.value[i];
-    //       console.log(obj['ServiceNo'])
-    //       // if (obj['ServiceNo'] == param) {
-    //       //   response.value.push(response.value[i]);
-    //       // }
-    //       // console.log(response[i]['ServiceNo']);
-    //       // console.log(obj);
-    //       responseSummary += '<tr>' + '<td>' + obj['ServiceNo'] + '<br>' + '</td>' + '<td>' + obj['AM_Peak_Freq'] + '<br>' + '</td>' + '<td>' + obj['AM_Offpeak_Freq'] + '<br>' + '</td>' +'</tr>'
-    //     }
-    //     $("#bus-content").html(responseSummary);
-  
-  
-  
-    //   });
-  
-    // }
+
   });
